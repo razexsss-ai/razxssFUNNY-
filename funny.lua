95c9c3e9f067d9f66766519d1e1ec6f982712ef1
razexsss-ai <razexs17@gmail.com>
2025-10-31 06:56:29 +0700

    Update funny.lua

--- funny.lua
+++ funny.lua
@@ -1,5 +1,17 @@
-return {
-    message = "Hello from Razexs!",
-    creator = "razexss-ai",
-    version = 1.0
-}
+-- funny.lua
+local url = "https://raw.githubusercontent.com/razexsss-ai/razxssFUNNY-/main/funny.json"
+
+local HttpGet = game and game.HttpGet or function() end
+local function fetchJSON()
+    local data = nil
+    local ok, res = pcall(function()
+        return game:HttpGet(url)
+    end)
+    if ok and res then
+        local json = game:GetService("HttpService"):JSONDecode(res)
+        return json
+    end
+    return {}
+end
+
+return fetchJSON()
